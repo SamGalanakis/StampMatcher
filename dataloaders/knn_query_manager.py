bd5d0918e81fcb7079c2352f016afe81a48dc73b
Sam Galanakis <samme.galanakis6@gmail.com>
2021-07-05 00:02:30 +0200

    Fix test

--- dataloaders/knn_query_manager.py
+++ dataloaders/knn_query_manager.py
@@ -1,43 +1,67 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader,Dataset
 import os
 import io
 import random
 from utils import custom_pil_loader
 from tqdm import tqdm
+import torchvision
 
 
+
+class TestLoaderForKnn(Dataset):
+    def __init__(self,root_dir,transforms):
+        super().__init__()
+        self.root_dir = root_dir
+        self.transforms = transforms
+        self.file_paths = os.listdir(root_dir)
+
+
+
+    def __len__(self):
+        return len(self.file_paths)
+
+
+    def __getitem__(self,idx):
+        img_path = os.path.join(self.root_dir,
+                                self.file_paths[idx])
+
+        img = custom_pil_loader(img_path)
+        img = self.transforms(img)
+        return img
 
 class KnnQueryManager:
-    def __init__(self,root_dir,transforms,model,device='cuda'):
+    def __init__(self,config,root_dir,transforms,model,device='cuda'):
         self.root_dir = root_dir
+        self.config = config
         self.transforms = transforms
         self.file_paths = os.listdir(root_dir)
         self.device = device
         model.eval()
-        model = model.to(device)
+        self.model = model.to(device)
         projections = []
+
+        dataset = TestLoaderForKnn(root_dir,transforms)
+        dataloader = DataLoader(dataset,batch_size=200,num_workers=8,pin_memory=True,shuffle=False)
         print('Creating index')
         with torch.no_grad():
-            for file_path in tqdm(self.file_paths):
-                img_path = os.path.join(self.root_dir,
-                                    file_path)
-                img = custom_pil_loader(img)
-                img = self.transforms(img).to(device)
-                projection = model(img)
+            for img in tqdm(dataloader):
+                img = img.to(device)
+                projection = self.model(img)
                 projections.append(projection)
-        self.projections = torch.stack(projections)
+        self.projections = torch.cat(projections)
     
 
     def query(self,img_path,k=1,verbose = True):
         img = custom_pil_loader(img_path)
-        img = self.transforms(img).to(self.device)
-        dist = torch.linalg.norm(self.projections - img, dim=-1)
+        img = self.transforms(img).to(self.device).unsqueeze(0)
+        embedding = self.model(img).squeeze()
+        dist = torch.linalg.norm(self.projections - embedding, dim=-1)
         knn = dist.topk(k, largest=False)
-        if verbose:
-            print('kNN dist: {}, index: {}'.format(knn.values, knn.indices))
-        return knn
-    
+        indices = knn[1].tolist()
+        paths = [os.path.join(self.root_dir,self.file_paths[ind]) for ind in indices]
+        return knn,paths
+
     def __len__(self):
         return len(self.file_paths)
 
--- test.py
+++ test.py
@@ -4,24 +4,44 @@ import os
 from torch.serialization import save 
 from dataloaders import KnnQueryManager, knn_query_manager
 from train import initialize_model,load_save,get_transforms
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+def get_query_manager(checkpoint_path):
+    save_dict = torch.load(checkpoint_path)
+    config = save_dict['config']
+    device = 'cuda'
+    model_dict = initialize_model(config,device)
+    model_dict = load_save(model_dict,config,save_dict,device)
+    model = torch.nn.Sequential(model_dict['encoder'],model_dict['projection_head'])
+    _ , test_transform = get_transforms(config)
 
 
-checkpoint_path  = ''
-test_dir = 'data/stamps/test_images'
-save_dict = torch.load(checkpoint_path)
-config = save_dict['config']
-device = 'cuda'
-model_dict = initialize_model(config,device)
-model_dict = load_save(model_dict,config,save_dict,device)
-model = torch.nn.Sequential([model_dict['encoder'],model_dict['projection_head']])
-_ , test_transform = get_transforms(config)
+
+    knn_query_manager = KnnQueryManager(config,config['train_dir'],test_transform,model,device)
+
+    return knn_query_manager
 
 
+if __name__ == '__main__':
+    test_dir = 'data/stamps/test_images'
+    checkpoint_path  = 'model_save/swift-butterfly-25_e19_model_dict.pt'
+    query_manager = get_query_manager(checkpoint_path)
+    for img_path in os.listdir(test_dir):
+        path = os.path.join(test_dir,img_path)
+        f, axarr = plt.subplots(4,1)
+        knn,paths = query_manager.query(path,k=3)
+        
+        plot_paths = [path] + paths
+        for index,match_path in enumerate(plot_paths):
+            img = Image.open(match_path)
+            img = img.resize([100,100])
+            axarr[index].imshow(img)
+        plt.show()
+      
 
-knn_query_manager = KnnQueryManager(config['train_dir'],test_transform,model,device)
 
-
-print()
+    
 
 
 
