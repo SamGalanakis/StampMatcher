ac012d7a2cc733ff24e7c932eac0d57d4d408b66
Sam Galanakis <samme.galanakis6@gmail.com>
2021-07-04 21:53:10 +0200

    Initialization and loading code

--- train.py
+++ train.py
@@ -10,13 +10,9 @@ from tqdm import tqdm
 import os 
 from model import MLP
 from dataloaders import knn_query_manager
+from utils import put_on_device
 
-
-def main():
-    run = wandb.init(config='configs/config.yaml',project = 'StampMatcher', entity='samme013')
-    config = wandb.config
-    
-
+def get_transforms(config):
     start_transforms_train = transforms.Compose([
         transforms.Resize((100, 100)),
         transforms.RandomCrop((80, 80)),
@@ -40,11 +36,14 @@ def main():
 
 
     train_transform = transforms.Compose([start_transforms_train,augmentations,end_transforms])
+
+
     test_transform = transforms.Compose([start_transforms_test,end_transforms])
 
-    loss_func = TripletLoss(margin = config['triplet_loss_margin'])
-    dataset = StampDataset('data/stamps/stamps',train_transform)
-    device = 'cuda'
+    return train_transform,test_transform
+
+
+def initialize_model(config,device):
     encoder = torchvision.models.resnet50(pretrained=True).to(device)
     encoder_out  = encoder.fc.out_features
     for parameter in encoder.parameters():
@@ -52,12 +51,35 @@ def main():
 
     projection_head  = MLP(encoder_out,config['hidden_sizes'],config['emb_dim'],nonlin=torch.nn.GELU(),residual=True).to(device)
 
+    return {'projection_head':projection_head,'encoder':encoder}
+
+
+def load_save(model_dict,config,save_dict,device):
+    model_dict['projection_head'].load_state_dict(save_dict['projection_head'])
+    for key,val in model_dict.items():
+        val = put_on_device(val,device)
+    return model_dict
+
+
+def main():
+    run = wandb.init(config='configs/config.yaml',project = 'StampMatcher', entity='samme013')
+    config = wandb.config
+    
+
+    train_transform,test_transform = get_transforms(config)
+
+
+    loss_func = TripletLoss(margin = config['triplet_loss_margin'])
+    dataset = StampDataset('data/stamps/stamps',train_transform)
+    device = 'cuda'
+    
+    model_dict = initialize_model(config)
 
 
     dataloader = DataLoader(dataset,batch_size=config['batch_size'],shuffle=True,num_workers=4,pin_memory=True,drop_last=True)
 
     optimizer = torch.optim.Adam(
-                projection_head.parameters(), lr=config["lr"])
+                model_dict['projection_head'].parameters(), lr=config["lr"])
 
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,patience=  config['patience_scheduler'], 
     factor=0.1, min_lr=1E-8,verbose=True)
@@ -72,8 +94,8 @@ def main():
         for batch_ind, batch in enumerate(tqdm(dataloader)):
             optimizer.zero_grad()
             batch = [x.to(device) for x in batch]
-            embeddings = [encoder(x) for x in batch]
-            projected_embeddings = [projection_head(x) for x in embeddings]
+            embeddings = [model_dict['encoder'](x) for x in batch]
+            projected_embeddings = [model_dict['projection_head'](x) for x in embeddings]
             loss = loss_func(*projected_embeddings)
             loss.backward()
             optimizer.step()
@@ -94,7 +116,7 @@ def main():
             savepath = os.path.join(
                 save_model_path, f"{wandb.run.name}_e{epoch}_model_dict.pt")
             
-            save_dict = {'config': config._items, "projection_head": projection_head.state_dict(
+            save_dict = {'config': config._items, "projection_head": model_dict['projection_head'].state_dict(
             )}
             torch.save(save_dict, savepath)
             last_save_path = savepath
--- utils.py
+++ utils.py
@@ -4,3 +4,11 @@ def custom_pil_loader(path):
     with open(path, "rb") as f:
         img = Image.open(f)
         return img.convert("RGB")
+
+def put_on_device(x,device):
+    try:
+        x = x.to(device)
+    except:
+        pass
+    return x
+        
